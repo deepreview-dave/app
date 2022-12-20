0ecd5975830a4c47c2fdaec3b37c85b18246f9b2
Horia Coman <horia141@gmail.com>
2022-12-20 14:28:17 +0200

    Bulma inline

--- src/components/ResultBlock.tsx
+++ src/components/ResultBlock.tsx
@@ -1,18 +1,22 @@
-import { EAppStatus, useAppState } from '../state/state';
+import { EAppStatus, useAppState } from "../state/state";
 
 export const ResultBlock = () => {
   const answer = useAppState((state) => state.answer);
   const isLoading = useAppState((state) => state.status === EAppStatus.LOADING);
 
   return (
-    <div className='message mt-6'>
-      <div className='message-header'>
+    <div className="message mt-6">
+      <div className="message-header">
         <p>Your review</p>
       </div>
-      <div className='message-body'>
-        {isLoading && <progress className="progress is-small is-primary" max="100">15%</progress>}
+      <div className="message-body">
+        {isLoading && (
+          <progress className="progress is-small is-primary" max="100">
+            15%
+          </progress>
+        )}
         {!isLoading && <span>{answer}</span>}
       </div>
     </div>
   );
-}
+};
