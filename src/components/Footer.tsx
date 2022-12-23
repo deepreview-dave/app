ea592b4db0e44751e3dadbaf96ea51eef49fb33b
Horia Coman <horia141@gmail.com>
2022-12-24 00:35:35 +0200

    Fix copy

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ export const Footer = () => {
     <footer className="footer p-6 mt-6">
       <div className="content has-text-centered">
         <p>
-          <strong>Auto Perf Review</strong>. The source code is licensed{" "}
+          <strong>Deep Review</strong>. The source code is licensed{" "}
           <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. With
           tasteful tracking.
         </p>
