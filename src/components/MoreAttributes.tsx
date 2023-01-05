356c5db881d3e1e3fa108146bd5741f38fe6b98c
devgabrielcoman <dev.gabriel.coman@gmail.com>
2023-01-05 17:55:59 +0000

    first attempt

--- src/components/MoreAttributes.tsx
+++ src/components/MoreAttributes.tsx
@@ -9,6 +9,9 @@ import { WorkAttribute } from "../business/common";
 import { useModalState } from "../state/modal.state";
 
 const AttributeModal = () => {
+  const ATTRIBUTE_NAME_MAX_LIMIT = 50;
+  const DETAILS_MAX_LENGTH = 500;
+
   const selectedType = useModalState((state) => state.selectedType);
   const isOpened = useModalState((state) => state.isOpened);
   const addAttribute = useAppState((state) => state.addAttribute);
@@ -17,6 +20,7 @@ const AttributeModal = () => {
   );
 
   const [text, setText] = useState("");
+  const [details, setDetails] = useState("");
 
   if (!selectedType) {
     return null;
@@ -24,6 +28,7 @@ const AttributeModal = () => {
 
   const onClose = () => {
     setText("");
+    setDetails("");
     closeAttributeModal();
   };
 
@@ -32,12 +37,18 @@ const AttributeModal = () => {
       uuid: generateUUID(),
       name: text,
       type: selectedType,
+      details,
     };
     addAttribute(attribute);
     onClose();
   };
 
   const isButtonDisabled = () => !text;
+
+  const currentAttributeRemainingLength = (): number =>
+    ATTRIBUTE_NAME_MAX_LIMIT - text.length;
+  const currentDetailsRemainingLength = (): number =>
+    DETAILS_MAX_LENGTH - details.length;
 
   return (
     <div className={isOpened ? "modal is-active" : "modal"}>
@@ -58,6 +69,7 @@ const AttributeModal = () => {
             {getAttributeDescription(selectedType).description}
           </div>
           <div className="field">
+            <label className="label">{selectedType}</label>
             <div className="control">
               <input
                 type="text"
@@ -65,9 +77,27 @@ const AttributeModal = () => {
                 required
                 value={text}
                 placeholder={getAttributeDescription(selectedType).placeholder}
-                maxLength={200}
+                maxLength={ATTRIBUTE_NAME_MAX_LIMIT}
                 onChange={(e) => setText(e.currentTarget.value)}
               ></input>
+              <div className="mt-1 has-text-grey-light is-size-7">
+                Remaining characters: {currentAttributeRemainingLength()}
+              </div>
+            </div>
+          </div>
+          <div className="field mt-2">
+            <label className="label">More details (optional):</label>
+            <div className="control">
+              <textarea
+                className="textarea"
+                value={details}
+                placeholder="Add specifics and other details (optional)"
+                maxLength={DETAILS_MAX_LENGTH}
+                onChange={(e) => setDetails(e.currentTarget.value)}
+              ></textarea>
+              <div className="mt-1 has-text-grey-light is-size-7">
+                Remaining characters: {currentDetailsRemainingLength()}
+              </div>
             </div>
           </div>
         </section>
@@ -124,17 +154,30 @@ export const MoreAttributes = () => {
       )}
       <div className="tags">
         {attributes.map((attr) => (
-          <span
-            key={attr.uuid}
-            className={"tag " + getAttributeDescription(attr.type).colorClass}
-          >
-            {attr.type}: {attr.name}
-            <button
-              disabled={!inputEnabled}
-              className="delete is-small"
-              onClick={() => removeAttribute(attr)}
-            ></button>
-          </span>
+          <article className="message is-small">
+            <div className="message-header">
+              <p>Small message</p>
+              <button className="delete is-small" aria-label="delete"></button>
+            </div>
+            <div className="message-body">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+              <strong>Pellentesque risus mi</strong>, tempus quis placerat ut,
+              porta nec nulla.Nullam gravida purus diam, et dictum{" "}
+              <a>felis venenatis</a> efficitur. Aenean ac{" "}
+              <em>eleifend lacus</em>, in mollis lectus.
+            </div>
+          </article>
+          // <span
+          //   key={attr.uuid}
+          //   className={"tag " + getAttributeDescription(attr.type).colorClass}
+          // >
+          //   {attr.type}: {attr.name} | {attr.details}
+          //   <button
+          //     disabled={!inputEnabled}
+          //     className="delete is-small"
+          //     onClick={() => removeAttribute(attr)}
+          //   ></button>
+          // </span>
         ))}
       </div>
       <AttributeModal />
